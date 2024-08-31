f76e49d0625f4c5560bbc1e0668af0f18cd0325d
Iriskhanov <iriskhanov94@mail.ru>
2024-09-01 00:46:52 +0300

    d1

--- weather/views.py
+++ weather/views.py
@@ -3,37 +3,6 @@ from django.shortcuts import render
 from . forms import CityForm
 
 
-def to_prepositional(city_name):
-    exceptions = {
-        'Грозный': 'Грозном',
-        'Москва': 'Москве',
-        'Сочи': 'Сочи',
-        'Тверь': 'Твери',
-        'Нижний Новгород': 'Нижнем Новгороде',
-
-    }
-
-    if city_name in exceptions:
-        return exceptions[city_name]    
-    if city_name.endswith('ый'):
-        return city_name[:-2] + 'ом'
-    elif city_name.endswith('ий'):
-        return city_name[:-2] + 'ем'
-    elif city_name.endswith('ая'):
-        return city_name[:-2] + 'ой'
-    elif city_name.endswith('е'):
-        return city_name
-    elif city_name.endswith('а'):
-        return city_name[:-1] + 'е'
-    elif city_name.endswith('я'):
-        return city_name[:-1] + 'е'
-    else:
-        return city_name + 'е'
-    
-def weather_forecast(city_name, temperature):
-    city_name_prepositional = to_prepositional(city_name)
-    return f"Погода в {city_name_prepositional}: {temperature} градусов."
-
 def fetch_weather_data(city, api_key):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}&lang=ru'
     try:
